3b6edb5c24ea95427b60456edb28024fd70926d7
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-09-05 23:11:42 +0530

    Comments describing test class

--- Pluralsight_Basics_of_Unit_Testing/MyClasses/MyClassesTest/FileProcessTest.cs
+++ Pluralsight_Basics_of_Unit_Testing/MyClasses/MyClassesTest/FileProcessTest.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+/******************************************************************
+ * Unit test is nothing more than a class with methods
+ * ***************************************************************/
 
 namespace MyClassesTest
 {
+    // They add these special attributes called TestClass and TestMethods.
+    // These special attributes came from namespace 'using Microsoft.VisualStudio.TestTools.UnitTesting;'
     [TestClass]
     public class FileProcessTest
     {
