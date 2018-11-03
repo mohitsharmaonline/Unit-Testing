e61b387a7a47506d28cb6bd6c0d14e3a1c99e159
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-11-03 21:55:46 +0530

    test for StringAssert.StartsWith method. The test passed.

---
+++ Pluralsight_Basics_of_Unit_Testing/MyClasses/MyClassesTest/StringAssertClassTest.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class StringAssertClassTest
+    {
+        [TestMethod]
+        public void ContainsTest()
+        {
+            string str1 = "John Kuhn";
+            string str2 = "Kuhn";
+
+            StringAssert.Contains(value: str1, substring: str2);
+        }
+    }
+}
