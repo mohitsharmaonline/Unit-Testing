1efddd8d6b5f78a21216b707b1b2d460bfc7432f
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-11-03 19:41:27 +0530

    Introduction to Assert.AreEqual

---
+++ Pluralsight_Basics_of_Unit_Testing/MyClasses/MyClassesTest/AssertClassTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class AssertClassTest
+    {
+        #region AreEqual/AreNotEqual Tests
+        [TestMethod]
+        [Owner("MohitS")]
+        public void AreEqualTest()
+        {
+            string str1 = "Paul";
+            string str2 = "Paul";
+
+            Assert.AreEqual(str1, str2);
+        }
+
+        #endregion
+    }
+}
