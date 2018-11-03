3bf667603c23cf6859dac2a1c898dd3f6af216e7
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-11-03 19:46:50 +0530

    New method got passed as it is raising expected exception.

--- Pluralsight_Basics_of_Unit_Testing/MyClasses/MyClassesTest/AssertClassTest.cs
+++ Pluralsight_Basics_of_Unit_Testing/MyClasses/MyClassesTest/AssertClassTest.cs
@@ -11,9 +11,20 @@ namespace MyClassesTest
         public void AreEqualTest()
         {
             string str1 = "Paul";
-            string str2 = "paul";
+            string str2 = "Paul";
 
             Assert.AreEqual(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("JohnK")]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void AreEqualsCaseSensitiveTest()
+        {
+            string str1 = "Paul";
+            string str2 = "paul";
+
+            Assert.AreEqual(str1, str2, false);
         }
 
 
