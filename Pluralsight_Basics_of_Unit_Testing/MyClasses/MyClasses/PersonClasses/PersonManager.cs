a1cd9c088f2538df4f3e474cd519976e1aaf6c35
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-11-03 20:55:01 +0530

    Basic infrastructure to use object type of test methods.

---
+++ Pluralsight_Basics_of_Unit_Testing/MyClasses/MyClasses/PersonClasses/PersonManager.cs
@@ -0,0 +1,71 @@
+﻿
+using System.Collections.Generic;
+
+namespace MyClasses.PersonClasses
+{
+    public class PersonManager
+    {
+        public Person CreatePerson(string first, string last, bool isSupervisor)
+        {
+            Person ret = null;
+            if(!string.IsNullOrWhiteSpace(first))
+            {
+                if(isSupervisor)
+                {
+                    ret = new Supervisor();
+                }
+                else
+                {
+                    ret = new Employee();
+                }
+
+                ret.FirstName = first;
+                ret.LastName = last;
+            }
+
+            return ret;
+        }
+
+        public List<Person> GetPeople()
+        {
+            List<Person> people = new List<Person>();
+
+            people.Add(new Person() { FirstName = "Paul", LastName = "Sheriff" });
+            people.Add(new Person() { FirstName = "John", LastName = "Kuhn" });
+            people.Add(new Person() { FirstName = "Jim", LastName = "Ruhl" });
+
+            return people;
+        }
+
+        public List<Person> GetSupervisors()
+        {
+            List<Person> people = new List<Person>();
+
+            people.Add(CreatePerson("Paul", "Sheriff", true));
+            people.Add(CreatePerson("Michael", "Krasowski", true));
+
+            return people;
+        }
+
+        public List<Person> GetEmployees()
+        {
+            List<Person> people = new List<Person>();
+
+            people.Add(CreatePerson("Steve", "Nystrom", false));
+            people.Add(CreatePerson("John", "Kuhn", false));
+            people.Add(CreatePerson("Jim", "Ruhl", false));
+
+            return people;
+        }
+
+        public List<Person> GetSupervisorsAndEmployees()
+        {
+            List<Person> people = new List<Person>();
+
+            people.AddRange(GetEmployees());
+            people.AddRange(GetSupervisors());
+
+            return people;
+        }
+    }
+}
---
+++ Pluralsight_Basics_of_Unit_Testing/MyClasses/MyClasses/PersonClasses/Supervisor.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Collections.Generic;
+
+namespace MyClasses.PersonClasses
+{
+    public class Supervisor : Person
+    {
+        public List<Employee> Employees { get; set; }
+    }
+}
