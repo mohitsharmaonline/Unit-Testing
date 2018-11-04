df45dea3280c049ebe446c9a91c7cba9cc4f39fd
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-11-04 12:26:46 +0530

    Method to get PersonManager classes CreatePerson method fully covered in code coverage.

---
+++ Pluralsight_Basics_of_Unit_Testing/MyClasses/MyClassesTest/PersonManagerTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses.PersonClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class PersonManagerTest
+    {
+        [TestMethod]
+        public void CreatePerson_OfTypeEmployeeTest()
+        {
+            PersonManager mgr = new PersonManager();
+            Person per;
+
+            per = mgr.CreatePerson(first: "Paul", last: "Sheriff", 
+                isSupervisor: false);
+
+            Assert.IsInstanceOfType(value: per, expectedType: typeof(Employee));
+
+        }
+    }
+}
